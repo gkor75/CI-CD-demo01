435d851403aab21dadd197f02d76b9bb87c0a36e
Grzegorz Kornacki <g.kornacki@f5.com>
2019-04-01 19:26:09 +0200

    Adding clean Jenkins Docker file

---
+++ Jenkins-container/build-Jenkins-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+IMAGE_NAME=jenkinscicd
+JENKINS_HOME=$HOME/jenkins_home
+docker ps |grep -q $IMAGE_NAME
+OUT=$?
+if [ $OUT -eq 0 ];then
+	echo "stopping $IMAGE_NAME..."
+	docker stop $IMAGE_NAME
+	docker rm $IMAGE_NAME
+fi
+docker images $IMAGE_NAME |grep -q $IMAGE_NAME 
+OUT=$?
+if [ $OUT -eq 0 ];then
+	echo "image $IMAGE_NAME found, removing..."
+	docker image rmi $IMAGE_NAME
+fi
+docker build -t $IMAGE_NAME:latest .
+
+docker run -d -v jenkins_home:$JENKINS_HOME -p 8080:8080 -p 50000:50000 --name $IMAGE_NAME $IMAGE_NAME:latest
